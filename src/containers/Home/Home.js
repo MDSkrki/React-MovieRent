8888ed3805f08e3eb1dd063923fab87d6a6473b0
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-04-13 22:59:46 +0200

    added lorem ipsum for style testing until we have the carrousels

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,7 +1,11 @@
+import './Home.css'
+import { LoremIpsum } from "../../components/LoremIpsum/LoremIpsum"
+
 export const Home = () => {
     return(
-        <>
+        <div className='Home'>
             <p>This is the Home Page</p>
-        </>
+            <LoremIpsum />
+        </div>
     )
 }
