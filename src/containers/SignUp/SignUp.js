446c1cea6222cca501a0abffe913d5f59a28d9fc
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-04-14 19:28:33 +0200

    cleaned some code and made successful form registration

--- src/containers/SignUp/SignUp.js
+++ src/containers/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { Input } from "../../components/Input/Input"
 
 
@@ -11,10 +11,6 @@ export const SignUp = () => {
         password: '',
     })
 
-    useEffect(() => {
-        console.log(form);
-    }, [form]);
-
     // Handlers
     const updateForm = (e) => {
         setForm({
@@ -24,15 +20,31 @@ export const SignUp = () => {
         console.log(e.target.name, e.target.value);
     }
 
-
-
+    const formSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await fetch('http://localhost:4000/api/user/register', {
+                method: 'POST',
+                body: JSON.stringify(form),
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })
+            const user = await res.json();
+            console.log(user);
+        } catch (error) {
+            console.log('User registration failed!');
+        }
+    }
     return (
         <>
             <h1>Sign Up</h1>
-            <form>
+            <form onSubmit={formSubmit}>
                 <Input inputType='username' update={updateForm} />
                 <Input inputType='email' update={updateForm} />
                 <Input inputType='password' update={updateForm} />
+
+                <button>Submit</button>
             </form>
         </>
     )
