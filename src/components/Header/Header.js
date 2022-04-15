1353d1ac417f75329ad8bb268d2cc8fae5b04170
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-04-15 22:20:54 +0200

    made header listen if user is logged or not

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,16 @@
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
 export const Header = () => {
+
+    const loggedState = useSelector((state) => state.user.logged)
+
     return (
         <header>
             <p>NiceFlix</p>
             <nav>
+                {loggedState && <Link to='/profile'>Profile</Link>}
                 <Link to="/">Home</Link>
             </nav>
         </header>
