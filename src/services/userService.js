542231358d485a4bf07ade63538e0dc3bdc38cc0
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-04-15 22:19:28 +0200

    created login service

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,3 +1,5 @@
+import jwtDecode from "jwt-decode";
+
 export const createUser = async (formData) => {
     try {
         const res = await fetch('http://localhost:4000/api/user/register', {
@@ -12,4 +14,30 @@ export const createUser = async (formData) => {
     } catch (error) {
         console.log('User registration failed!');
     }
+}
+
+export const loginUser = async (formData) => {
+    try {
+        const res = await fetch('http://localhost:4000/api/user/login', {
+            method: 'POST',
+            body: JSON.stringify(formData),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+        const {token} = await res.json();
+        if (token) return tokenDecoder(token);
+    } catch (error) {
+        console.log('User Login has failed');
+    }
+}
+
+const tokenDecoder = (token) => {
+    try {
+        const decoded = jwtDecode(token);
+        return decoded.id;
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
