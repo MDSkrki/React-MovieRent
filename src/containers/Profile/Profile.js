a4bb05cb65460feadffe2488993d2247fb46a63c
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-04-16 00:07:58 +0200

    profile much more ready to party

--- src/containers/Profile/Profile.js
+++ src/containers/Profile/Profile.js
@@ -1,9 +1,19 @@
+import { useSelector } from 'react-redux'
 import './Profile.css'
 
 export const Profile = () => {
+
+    const userState = useSelector((state) => state.user);
+
     return (
         <>
-        
+            <div>
+                <h1>Welcome {userState.username}! </h1>
+            </div>
+
+            <div>
+                <h2>Here are your rented movies:</h2>
+            </div>
         </>
     )
 }
