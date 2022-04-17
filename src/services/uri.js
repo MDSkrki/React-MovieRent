a47a58fcaa15af2f828ad20f5f5ef46fa78e424a
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-04-17 16:59:50 +0200

    added paths to TMDB for gets

--- src/services/uri.js
+++ src/services/uri.js
@@ -1,2 +1,9 @@
+
+const apiKey = '88a1358f8071bd268e053f9a51588c71';
+export const baseImgPath = 'https://image.tmdb.org/t/p/w500/';
+
 export const userLoginPath = 'http://localhost:4000/api/user/login';
 export const userSignUpPath = 'http://localhost:4000/api/user/register';
+
+export const getSingleMovieTMDB = `https://api.themoviedb.org/3/movie/550?api_key=${apiKey}&language=en-US`;
+export const getPopularMoviesTMDB = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
