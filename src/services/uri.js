54edb98a2a738c1842d3aba4e412a9dab3d1d292
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-04-19 22:55:05 +0200

    changed some uris to use heroku basepath

--- src/services/uri.js
+++ src/services/uri.js
@@ -2,11 +2,14 @@
 const apiKey = '88a1358f8071bd268e053f9a51588c71';
 export const baseImgPath = 'https://image.tmdb.org/t/p/w500/';
 
-export const userLoginPath = 'http://localhost:4000/api/user/login';
-export const userSignUpPath = 'http://localhost:4000/api/user/register';
+const localhost = 'http://localhost:4000/';
+const heroku = 'https://mdskrki-api-movie-database.herokuapp.com/';
+
+export const userLoginPath = heroku + 'api/user/login';
+export const userSignUpPath = heroku + 'api/user/register';
 
 export const getSingleMovieTMDB = `https://api.themoviedb.org/3/movie/550?api_key=${apiKey}&language=en-US`;
 export const getPopularMoviesTMDB = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
 
-export const postRentPath = 'http://localhost:4000/api/rent/';
-export const getRentsPath = 'http://localhost:4000/api/rent/';
+export const postRentPath = heroku + 'api/rent/';
+export const getRentsPath = heroku + 'api/rent/';
