d0bd4bc96c86390cf9a52f9edeaab5bc48d5f61f
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-04-19 22:39:45 +0200

    sends data to movie detail through pathparams AND navigation

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,12 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+import { getMovie } from '../../services/movieService';
 import { baseImgPath } from '../../services/uri';
 import './MovieCard.css';
 
 export const MovieCard = ({movie}) => {
 
+    // Hook
+    const navigate = useNavigate();
+
+    // Handler
+    const clickHandler = async () => {
+        const movieDetail = await getMovie(movie.id);
+        navigate('/movie/' + movie.id, {state: movieDetail});
+    }
+
     return (
         <div className='MovieCard'>
             <h2>{movie.title}</h2>
-            <img src={baseImgPath + movie.poster_path}  alt='img'/>
+            <img src={baseImgPath + movie.poster_path}  alt='img' onClick={clickHandler}/>
         </div>
     )
 }
