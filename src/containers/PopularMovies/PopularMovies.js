956ea134b3f4379f8a0e56f0781c75746a1cd6c7
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-04-19 22:40:04 +0200

    removed weird button

--- src/containers/PopularMovies/PopularMovies.js
+++ src/containers/PopularMovies/PopularMovies.js
@@ -5,13 +5,8 @@ import { MovieCard } from "../../components/MovieCard/MovieCard";
 export const PopularMovies = () => {
     const movies = useSelector((state) => state.movie.popular)
 
-    const buttonHandler = () => {
-        console.log(movies);
-    }
-
     return (
         <>
-            <button onClick={buttonHandler}>Click Me!</button>
             <div className="PopularMovies">
                 {movies && movies.map((movie) => {
                     return (
