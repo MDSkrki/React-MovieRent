b9006de939f021d6bea3aebca7d2f2556860643c
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-04-19 22:43:09 +0200

    big problems kind of solved

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,20 +1,51 @@
 import { apiConsumer } from "./apiConsumer";
-import { getPopularMoviesTMDB, getSingleMovieTMDB } from "./uri";
+import { getPopularMoviesTMDB, getRentsPath, postRentPath } from "./uri";
 
 const baseImgPath = 'https://image.tmdb.org/t/p/w500/';
+const apiKey = '88a1358f8071bd268e053f9a51588c71';
 
-export const getMovie = async () => {
-    const movie = await apiConsumer('GET', getSingleMovieTMDB);
+export const getMovie = async (id) => {
+    const movie = await apiConsumer('GET', `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US`);
     return {
         title: movie.title,
         img: baseImgPath + movie.poster_path,
         releaseDate: movie.release_date,
-        genre: movie.genre[0].name,
+        price: '4€',
     };
 }
 
 export const getPopularMovies = async () => {
-    const movies = await apiConsumer('GET', getPopularMoviesTMDB);
-    return movies;
+    try {
+        const movies = await apiConsumer('GET', getPopularMoviesTMDB);
+        return movies;
+    } catch (error) {
+        console.log(error);
+    }
 }
 
+export const postRent = async (movie) => {
+    try {
+        const rent = await apiConsumer('POST', postRentPath, movie);
+        return rent
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getRents = async (id) => {
+    if (!id) {
+        try {
+            const rents = await apiConsumer('GET', getRentsPath);
+            return rents;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    try {
+        const rents = await apiConsumer('GET', getRentsPath + `?idUser=${id}`);
+        return rents;
+    } catch (error) {
+        console.log(error);
+    }
+}
